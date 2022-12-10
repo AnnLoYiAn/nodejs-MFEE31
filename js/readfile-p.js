da5ca2ce9d888a3827bf06b25614f620529a06df
AnnLoYiAn <anne.luoyian@gmail.com>
2022-12-10 12:19:48 +0800

    修改readfile-p.js

--- js/readfile-p.js
+++ js/readfile-p.js
@@ -1,9 +1,12 @@
 const fs = require('fs')
 
 let file = new Promise((resolve, reject) => {
-    fs.readFile('test.txt', 'utf-8', () => {
-        resolve('success')
-        reject('failure')
+    fs.readFile('test.txt', 'utf-8', (err, data) => {
+        if(err) {
+            reject('發生錯誤')
+        } else {
+            resolve('success')
+        }
     })
 })
 
@@ -11,7 +14,7 @@ let file = new Promise((resolve, reject) => {
 
 
 file.then((data) => {
-    console.log('success', data)
+    console.log('成功讀到資料:', data)
 }).catch((error) => {
-    console.log('發生錯誤', err)
+    console.log('發生錯誤了', err)
 })
