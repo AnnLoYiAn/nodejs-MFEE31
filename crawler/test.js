d5941ad89552790a344a064d7bcdddb0da31e94d
AnnLoYiAn <anne.luoyian@gmail.com>
2022-12-17 12:07:47 +0800

    test.js

--- crawler/test.js
+++ crawler/test.js
@@ -1,15 +1,17 @@
 const mysql2 = require('mysql2/promise');
+// npm i dotenv
+require('dotenv').config();
 
 (async () => {
     const connection = await mysql2.createConnection({
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'admin',
-        password: '',
-        database: 'stock_mfee31',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PWD,
+        database: process.env.DB_NAME,
     });
-    let result = await connection.query('SELECT * FROM `stocks`');
-    let data = result[0];
+    let [data, fields] = await connection.query('SELECT * FROM `stocks`');
+    
     console.log(data);
     connection.end();
     // 要記得end connection (掛電話的概念)
